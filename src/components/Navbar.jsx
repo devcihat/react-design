a94ea4146bd322595c31ae63e197987d4bbd4257
devcihat <cihattsungu@outlook.com>
2021-01-23 23:48:44 +0300

    LOGO modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
               <NavLink className="navbar-brand" to="/">
-                Logo
+                <i class="fab fa-react" />
               </NavLink>
               <button
                 className="navbar-toggler"
