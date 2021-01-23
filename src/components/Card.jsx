c7c2033a3a9aa032c9b2dbe0dee1bfe42da1bb39
devcihat <cihattsungu@outlook.com>
2021-01-23 18:13:06 +0300

    1 file modified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,8 +8,8 @@ function Card(props) {
         <div className="card">
           <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc} />
           <div className="card-body">
-            <h5 className="card-title font-weight-bold">{props.title}</h5>
-            <p className="card-text">
+            <h5 className="card-title fw-bold">{props.title}</h5>
+            <p className="card-text fw-lighter">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
