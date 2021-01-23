bc63e2f5e72f2cb0856b48ab9da1a1759f7ca11c
devcihat <cihattsungu@outlook.com>
2021-01-23 16:30:36 +0300

    1 binaries uploaded

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Card from "./Card";
 
 function Service() {
-  return <h1>Welcome Service Page</h1>;
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center">Our Services</h1>
+      </div>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row gy-4">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Service;
