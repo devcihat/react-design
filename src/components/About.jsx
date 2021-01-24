8009f5a05fa4cedae93d9fd278d593328863402c
devcihat <cihattsungu@outlook.com>
2021-01-24 18:39:53 +0300

    1 file modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,16 +31,102 @@ function About() {
           </div>
         </div>
       </div>
-      <div style={{ background: "black" }} className="col-12 mt-5">
-        <h3 className="text-white text-center">Merhaba</h3>
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>{" "}
-        <h3 className="text-white text-center">Merhaba</h3>
+      <div style={{ background: "black" }} className="text-center mt-5">
+        <div class="row pt-5">
+          <div class="col-lg-4">
+            <svg
+              class="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 140x140"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+
+            <h2 className="text-white">Heading</h2>
+            <p className="text-white">
+              Donec sed odio dui. Etiam porta sem malesuada magna mollis
+              euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+              Praesent commodo cursus magna.
+            </p>
+            <p className="text-white">
+              <button class="btn-get-started  btn-secondary" role="button">
+                View details »
+              </button>
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <svg
+              class="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 140x140"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+
+            <h2 className="text-white">Heading</h2>
+            <p className="text-white">
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula,
+              eget lacinia odio sem nec elit. Cras mattis consectetur purus sit
+              amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor
+              mauris condimentum nibh.
+            </p>
+            <p className="text-white">
+              <button class="btn-get-started btn-secondary" role="button">
+                View details »
+              </button>
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <svg
+              class="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 140x140"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+
+            <h2 className="text-white">Heading</h2>
+            <p className="text-white">
+              Donec sed odio dui. Cras justo odio, dapibus ac facilisis in,
+              egestas eget quam. Vestibulum id ligula porta felis euismod
+              semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+              condimentum nibh, ut fermentum massa justo sit amet risus.
+            </p>
+            <p className="text-white">
+              <button class="btn-get-started btn-secondary" role="button">
+                View details »
+              </button>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
