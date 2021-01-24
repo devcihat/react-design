143a11cff67cae92489edda2d4462e7aa3451589
devcihat <cihattsungu@outlook.com>
2021-01-24 18:49:39 +0300

    1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -128,6 +128,36 @@ function About() {
           </div>
         </div>
       </div>
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <div className="row">
+            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+              <h3 className="fw-bold text-center">
+                Estimate For <br /> Future Projects
+              </h3>
+              <p className="text-muted text-center mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation.
+              </p>
+              
+              <div className="mt-3" />
+            </div>
+            <div className="col-lg-6 order-1 order-lg-2 header-img pt-5">
+              <lottie-player
+                src="https://assets4.lottiefiles.com/packages/lf20_czeYL8.json"
+                background="red"
+                speed="1"
+                loop
+                autoplay
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
